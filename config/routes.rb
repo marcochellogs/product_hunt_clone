371138575633c0fa2bed3e003944ddbe039fc41b
marco checchi <marco.checchi@gmail.com>
2021-10-16 11:16:39 +0200

    added github action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index' # And remove the generated route `products/index`
+  resources :products, only: [:new, :create]
 end
